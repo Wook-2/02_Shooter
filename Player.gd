ff4e14bb6b3e7ad4f19022aa8143a03fd9ea6a1c
Wook-2 <bwson98@gmail.com>
2020-10-26 15:50:34 +0900

    create prototype

---
+++ Player.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+var movespeed = 500
+var bulletspeed = 2000
+var bullet = preload("res://Bullet.tscn")
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(_delta):
+	var motion = Vector2()
+	
+	if Input.is_action_pressed("up"):
+		motion.y -= 1
+	if Input.is_action_pressed("down"):
+		motion.y += 1
+	if Input.is_action_pressed("left"):
+		motion.x -= 1
+	if Input.is_action_pressed("right"):
+		motion.x += 1
+	
+	motion = motion.normalized()
+	motion = move_and_slide(motion * movespeed)
+	
+	look_at(get_global_mouse_position())
+	if Input.is_action_pressed("LMB"):
+		fire()
+
+func fire():
+	var bullet_instance = bullet.instance()
+	bullet_instance.position = get_global_position()
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply_impulse(Vector2(), Vector2(bulletspeed, 0).rotated(rotation))
+	get_tree().get_root().call_deferred("add child", bullet_instance)
